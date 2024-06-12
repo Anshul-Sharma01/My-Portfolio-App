6675b40761728457826781a814e43f3b291482b8
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-06-12 23:06:05 +0530

    Added another Contact Me section, much good in UI

--- src/components/Contact-Us/Contact.jsx
+++ src/components/Contact-Us/Contact.jsx
@@ -4,48 +4,27 @@ import { VscMail } from "react-icons/vsc";
 function Contact(){
     return(
         <>
-            <div className="m-20 p-20 font-sans border-ridge border-2 flex flex-col gap-10">
-                <h1 className="text-center text-2xl bg-white text-black p-3 font-bold rounded-lg mt-1">Contact Me</h1>
-                <form>
-                    <label htmlFor="nm">Name :</label>
-                    <br />
-                    <input type="text" id="nm" placeholder="Your Name ? " />
-                    <br />
-                    <br />
-                    <label htmlFor="mail">Email : </label>
-                    <br />
-                    <input type="email" placeholder="Your Email ?" id="mail" />
-                    <br />
-                    <br />
-                    <label htmlFor="message">Message : </label>
-                    <br />
-                    <textarea id="message" rows="5" cols="40" placeholder="Enter your message">
-                    </textarea>
-                    <br />
-                    <br />
-                    <button type="submit" className="border-solid border-white rounded-lg border-2 px-4 py-2 ">Submit</button>
-                </form>
-
-                <h2 className="text-xl font-sans bg-white text-black font-bold text-center rounded-lg p-3">My Online Presence</h2>
-                <div className='flex gap-20'>
-                    <div  className='border-solid border-white border-spacing-2 border-2 h-3/4 w-9 p-2'>
-                        <a href="https://www.linkedin.com/in/anshulsharma29/" target='_blank'>
-                            <FaLinkedinIn/>
-                        </a>
-                    </div>
-                    <div  className='border-solid border-white border-spacing-2 border-2 h-3/4 w-9 p-2'>
-                        <a href="https://github.com/Anshul-Sharma01/" target='_blank'>
-                            <FaGithub/>
-                        </a>
-                    </div>
-                    <div  className='border-solid border-white border-spacing-2 border-2 h-3/4 w-9 p-2'>
-                        <a href="mailto:sharma.ansh2926@gmail.com" target='_blank' >
-                            <VscMail/>
-                        </a>
-                    </div>
-
+            <section className="bg-slate-900">
+                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+                    
+                    <form action="#" className="space-y-8">
+                        <div>
+                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                        </div>
+                        <div>
+                            <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Your good name" required />
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                            <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                        </div>
+                        <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                    </form>
                 </div>
-            </div>
+            </section>
         </>
     )
 }
