9aa8cf5076a7c247f20c3410da4709de92e5053a
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-06-14 11:54:04 +0530

    Enhanced some UI

--- src/components/Contact-Us/Contact.jsx
+++ src/components/Contact-Us/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { CiMail } from 'react-icons/ci';
 import { FaDiscord, FaGithub, FaLinkedin, FaTelegram, FaVoicemail } from 'react-icons/fa';
 
 function Contact() {
@@ -45,27 +46,7 @@ function Contact() {
     return (
         <>
             <section className="bg-slate-900 m-50">
-                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
-
-                    <form onSubmit={handleSubmit} className="space-y-8">
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                            <input type="email" id="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-                        </div>
-                        <div>
-                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                            <input type="text" id="name" name='Name' value={name} onChange={(e) => setName(e.target.value)} className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Your good name" required />
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                            <textarea id="message" rows="6" name='message' value={message} onChange={(e) => setMessage(e.target.value)} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-                        </div>
-                        {error && <div className="text-red-500">{error}</div>}
-                        <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-                    </form>
-                </div>
-                <div>
+                <div className='pt-10'>
                     <h2 className=' text-xl tracking-tight font-extrabold text-center text-white pt-1  p-5 m-5'>My Online Presence</h2>
                     <div className='pb-10'>
                         <section className="flex gap-10 justify-center items-center">
@@ -108,7 +89,7 @@ function Contact() {
                         </a>
                         
                         <a
-                            href="https://discord.com/chat/anshulsharma_30663"
+                            href="https://discordapp.com/users/anshulsharma_30663"
                             className="group flex justify-center p-2 rounded-md drop-shadow-xl bg-gradient-to-r from-gray-800 to-black text-white font-semibold hover:translate-y-3 transition-all duration-500 hover:from-[#331029] hover:to-[#310413]" target='_blank'
                         >
                             <svg
@@ -136,7 +117,7 @@ function Contact() {
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
                             >
-                            <FaVoicemail/>
+                            <CiMail/>
                             </svg>
                             <span
                             className="absolute opacity-0 group-hover:opacity-100 group-hover:text-gray-700 group-hover:text-sm group-hover:-translate-y-10 duration-700"
@@ -148,6 +129,27 @@ function Contact() {
 
                     </div>
                 </div>
+                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+
+                    <form onSubmit={handleSubmit} className="space-y-8">
+                        <div>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                            <input type="email" id="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                        </div>
+                        <div>
+                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                            <input type="text" id="name" name='Name' value={name} onChange={(e) => setName(e.target.value)} className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Your good name" required />
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                            <textarea id="message" rows="6" name='message' value={message} onChange={(e) => setMessage(e.target.value)} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                        </div>
+                        {error && <div className="text-red-500">{error}</div>}
+                        <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 hover:bg-blue-900">Send message</button>
+                    </form>
+                </div>
+                
             </section>
         </>
     );
--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -4,9 +4,9 @@ function TechStack(){
     return(
         <div className="hddn_class m-20">
             <h1 className="text-2xl font-bold font-sans p-4 ml-5 text-left pb-6">What I Know </h1>
-            <div className="flex flex-wrap gap-20 ml-10">
+            <div className="flex flex-wrap gap-20 ml-10 text-2xl">
                 <div className="w-[250px] rounded-md border p-3 bg-[#1c1b23]">
-                    <div className="p-4">
+                    <div className="p-2">
                         <h1 className="m-2 rounded-xl p-3 text-center text-white">Frontend</h1>
                         <div className="mt-4">
                             <span className="mb-2 mr-2 inline-block rounded-md border-2 border-solid border-slate-700 bg-[#14131a] px-4 py-2 text-[10px] font-semibold text-white"> Html </span>
@@ -18,7 +18,7 @@ function TechStack(){
                     </div>
                 </div>
                 <div className="w-[250px] p-3 rounded-md border bg-[#1c1b23]">
-                    <div className="p-4">
+                    <div className="p-2">
                         <h1 className="m-2 rounded-xl p-3 text-center text-white">Backend</h1>
                         <div className="mt-4">
                             <span className="mb-2 mr-2 inline-block rounded-md border-2 border-solid border-green-700 bg-[#14131a] px-4 py-2 text-[10px] font-semibold text-white"> Node Js </span>
@@ -30,7 +30,7 @@ function TechStack(){
                     </div>
                 </div>
                 <div className="w-[250px] p-3 rounded-md border bg-[#1c1b23]">
-                    <div className="p-4">
+                    <div className="p-2">
                         <h1 className="m-2 rounded-xl p-3 text-center text-white">Database</h1>
                         <div className="mt-4">
                             <span className="mb-2 mr-2 inline-block rounded-md border-2 border-solid border-yellow-700 bg-[#14131a] px-4 py-2 text-[10px] font-semibold text-white"> My SQL</span>
@@ -40,7 +40,7 @@ function TechStack(){
                     </div>
                 </div>
                 <div className="w-[250px] p-3 rounded-md border bg-[#1c1b23]">
-                    <div className="p-4">
+                    <div className="p-2">
                         <h1 className="m-2 rounded-xl p-3 text-center text-white">Programming</h1>
                         <div className="mt-4">
                             <span className="mb-2 mr-2 inline-block rounded-md border-2 border-solid border-green-700 bg-[#14131a] px-4 py-2 text-[10px] font-semibold text-white"> Python</span>
@@ -48,6 +48,8 @@ function TechStack(){
                                 C </span>
                             <span className="mb-2 mr-2 inline-block rounded-md border-2 border-solid border-blue-700 bg-[#14131a] px-4 py-2 text-[10px] font-semibold text-white"> 
                             C++ </span>
+                            <span className="mb-2 mr-2 inline-block rounded-md border-2 border-solid border-orange-700 bg-[#14131a] px-4 py-2 text-[10px] font-semibold text-white"> 
+                            Git </span>
                         </div>
                     </div>
                 </div>
