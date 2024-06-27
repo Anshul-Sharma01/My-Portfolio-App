4e056a298cc84421dfb19953bbdffc807f03ce79
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-06-27 13:59:28 +0530

    added some endpoint for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,9 @@
 function Navbar(){
     return(
         <>
-            <div className="fixed hidden md:flex justify-around p-5 mb-2 w-full items-center  top-0 z-10 bg-black bg-opacity-50 backdrop-blur-md ">
-                <div></div>
-                <ul className="sm:flex justify-between gap-10 list-none">
-                    <li className="hover:border-b-4 hover:border-b-blue-400 duration-75 hover:text-lg"><a href="#hero">Home</a></li>
+            <div className="fixed flex justify-around p-5 mb-2 w-full items-center  top-0 z-10 bg-black bg-opacity-50 backdrop-blur-md ">
+                <ul className="flex justify-between gap-10 list-none">
+                    <li className="hover:border-b-4 hover:border-b-blue-400 duration-75 hover:text-lg"><a>Home</a></li>
                     <li className="hover:border-b-4 hover:border-b-blue-400 duration-75 hover:text-lg">
                         <a href="https://github.com/Anshul-Sharma01" target='_blank' rel='noreferrer'>
                             Github
@@ -16,11 +15,11 @@ function Navbar(){
                             LinkedIn
                         </a>
                     </li>
-                    <li className="hover:border-b-4 hover:border-b-blue-400 duration-75 hover:text-lg"><a href="#about">About</a></li>
-                    <li className="hover:border-b-4 hover:border-b-blue-400 duration-75 hover:text-lg"><a href="#contact">Contact Me</a></li>
+                    <li className="hover:border-b-4 hidden md:block hover:border-b-blue-400 duration-75 hover:text-lg"><a href="#about">About</a></li>
+                    <li className="hover:border-b-4 hidden md:block hover:border-b-blue-400 duration-75 hover:text-lg"><a href="#contact">Contact Me</a></li>
                 </ul>
 
-                <div className='flex justify-between gap-5'>
+                <div className='hidden md:flex justify-between gap-5 '>
                     <button className="border-solid border-2 border-white font-semibold px-6 py-2 rounded-lg hover:text-blue-500 hover:bg-white">
                         <a href="#projects">My Projects</a>
                     </button>
